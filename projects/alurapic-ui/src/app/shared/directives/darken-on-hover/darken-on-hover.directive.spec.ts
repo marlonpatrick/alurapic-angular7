676e43f019c42fba12863410b89a1b966a59f24e
Marlon Patrick <marlonpatrick10.cadastros@gmail.com>
2018-11-21 16:25:10 -0300

    test file fix: darken-on-hover.directive.spec.ts

--- projects/alurapic-ui/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.spec.ts
+++ projects/alurapic-ui/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.spec.ts
@@ -2,7 +2,7 @@ import { DarkenOnHoverDirective } from './darken-on-hover.directive';
 
 describe('DarkenOnHoverDirective', () => {
   it('should create an instance', () => {
-    const directive = new DarkenOnHoverDirective();
+    const directive = new DarkenOnHoverDirective(null, null);
     expect(directive).toBeTruthy();
   });
 });
