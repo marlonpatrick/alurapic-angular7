c35d42d7cabd415e1c30d64022a7d910d0664dc6
Marlon Patrick <marlonpatrick10.cadastros@gmail.com>
2018-11-23 12:21:44 -0300

    Implementing Reactive Forms in Sign up page, with custom validator

--- projects/alurapic-ui/src/app/access-control/sing-up/sign-up.module.ts
+++ projects/alurapic-ui/src/app/access-control/sing-up/sign-up.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SignUpPage } from './sign-up.page';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ValidationMessageModule } from '../../shared/components/validation-message/validation-message.module';
 
 @NgModule({
   declarations: [SignUpPage],
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule, // TODO: remove
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ValidationMessageModule
   ]
 })
 export class SignUpModule { }
--- projects/alurapic-ui/src/app/access-control/sing-up/sign-up.page.ts
+++ projects/alurapic-ui/src/app/access-control/sing-up/sign-up.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { userNameValidator } from './validators/user-name.validator';
 
 @Component({
   templateUrl: './sign-up.page.html',
@@ -6,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+  pageForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.pageForm = this.formBuilder.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      fullName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]
+      ],
+      userName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+          userNameValidator
+        ]
+      ],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(16)
+        ]
+      ]
+    });
   }
-
 }
